4fca093aa2ffa378e152d5e1274bfc590d74c368
adilraees786 <adilraees911@gmail.com>
2024-03-25 16:33:00 +0500

    Nested js

---
+++ app.js
@@ -0,0 +1,190 @@
+
+//NUMBER PATTERN
+
+
+
+for(i=0; i<=7; i++){
+    for(j=1; j<=i; j++){
+        document.write(j, ' ' )
+    }
+
+    document.write('<br>')
+}
+
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+for(i=7; i>=1; i--){
+    for(j=i; j>=1; j--){
+        document.write(j, ' ' )
+    }
+
+    document.write('<br>')
+}
+
+
+
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+document.write('<center>');
+
+
+for(i=0; i<=7; i++){
+    for(j=1; j<=i; j++){
+        document.write(j, ' ' )
+    }
+
+    document.write('<br>')
+}
+
+
+
+
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+document.write('<center>');
+                    
+for(i=7; i>=1; i--){
+    for(j=i; j>=1; j--){
+        document.write(j, ' ' )
+    }
+
+    document.write('<br>')
+}
+
+
+
+
+
+
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+// for(a=7; a>=1; a--){
+//     for(b=a; b>=1; b--){
+
+//         for(c=a; c>=b; c--){
+//             document.write('c')
+//         }
+//     }
+//     document.write("<br>")
+// }
+
+
+
+
+
+
+for(i=0; i<=7; i++){
+    for(j=1; j<=i; j++){
+        document.write(j, ' ' )
+    }
+
+    document.write('<br>')
+}
+
+
+for(i=6; i>=1; i--)
+{
+        for(j=i; j>=1; j--){
+            document.write(j, ' ' )
+        }
+    
+        document.write('<br>')
+    }
+    
+
+
+  //Star Pattern 
+
+
+
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+
+
+  for(x=0; x<=7; x++){
+    for(y=1; y<=x; y++){
+        document.write( ' ' +'*')
+   
+    }
+    document.write("<br>")
+
+  }
+
+
+
+  document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+document.write('<br>')
+
+document.write("<center>")
+
+
+  for(x=7; x>=1; x--){
+    for(y=x; y>=1; y--){
+        document.write( ' ' +'*')
+   
+    }
+    document.write("<br>")
+
+  }
+
+
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  
+  
+  
+  for(x=7; x>=1; x--){
+    for(y=x; y>=1; y--){
+        document.write( ' ' +'*')
+   
+    }
+    document.write("<br>")
+
+  }
+
+        for(x=2; x<=7; x++){
+    for(y=1; y<=x; y++){
+        document.write( ' ' +'*')
+   
+    }
+    document.write("<br>")
+
+  }
+
+
+  for(x=0; x<=7; x++){
+    for(y=1; y<=x; y++){
+        document.write( ' ' +'*')
+   
+    }
+    document.write("<br>")
+
+  }
+
+  for(x=6; x>=1; x--){
+    for(y=x; y>=1; y--){
+        document.write( ' ' +'*')
+   
+    }
+    document.write("<br>")
+
+  }
